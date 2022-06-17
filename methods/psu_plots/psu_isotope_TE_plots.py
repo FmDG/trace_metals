a00b7ca5bd84ff283b55bae9d3066cf5e856af6d
FmDG <f.m.degraaf@qmul.ac.uk>
2022-06-17 15:26:52 +0100

    ADD FILES TO PLOT PSU DATA

---
+++ methods/psu_plots/psu_isotope_TE_plots.py
@@ -0,0 +1,91 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import os
+
+os.chdir("../..")
+
+ox_1208 = pd.read_csv("data/ODP1208_cibs.csv")
+ox_1209 = pd.read_csv("data/ODP1209_cibs.csv")
+
+te_1208 = pd.read_csv("data/1208_TraceMetals.csv")
+te_1209 = pd.read_csv("data/1209_TraceMetals.csv")
+
+psu_1208 = pd.read_csv("data/PSU_Solver/PSU_data_1208.csv")
+psu_1209 = pd.read_csv("data/PSU_Solver/PSU_data_1209.csv")
+
+min_age, max_age = 2500, 3200
+colour_1208, colour_1209 = "#2D7DD2", "#FF8C42"
+
+fig, axs = plt.subplots(4, 1, sharex=True)
+# Remove horizontal space between axes
+fig.subplots_adjust(hspace=0)
+
+fig.suptitle("Comparison of {} and Mg/Ca".format(r'$\delta^{18}$O'))
+
+axs[0].plot(ox_1208.age_ka, ox_1208.d18Oadj, marker='+', color=colour_1208, label='ODP 1208')
+axs[0].plot(ox_1209.age_ka, ox_1209.d18Oadj, marker='+', color=colour_1209, label="ODP 1209")
+axs[0].legend(loc='upper left', shadow=False, frameon=False, bbox_to_anchor=(0.90, 1.05))
+axs[0].set(xlim=[min_age, max_age], ylabel='{} ({})'.format(r'$\delta^{18}$O', u"\u2030"), xlabel='Age (ka)')
+axs[0].invert_yaxis()
+axs[0].spines['right'].set_visible(False)
+
+axs[1].plot(te_1208.age_ka, te_1208.MgCa, marker='+', color=colour_1208, label="ODP 1208")
+axs[1].plot(te_1209.age_ka, te_1209.MgCa, marker='+', color=colour_1209, label="ODP 1209")
+axs[1].set(ylabel='{} ({})'.format('Mg/Ca', "mmol/mol"))
+axs[1].yaxis.set(ticks_position="right", label_position='right')
+axs[1].spines['left'].set_visible(False)
+
+axs[2].plot(psu_1208.age_ka, psu_1208.temp, color=colour_1208, linestyle='-', alpha=1)
+axs[2].fill_between(psu_1208.age_ka, psu_1208.temp_min1, psu_1208.temp_add1, alpha=0.1, facecolor=colour_1208)
+axs[2].plot(psu_1209.age_ka, psu_1209.temp, color=colour_1209, linestyle='-', alpha=1)
+axs[2].fill_between(psu_1209.age_ka, psu_1209.temp_min1, psu_1209.temp_add1, alpha=0.1, facecolor=colour_1209)
+axs[2].set(ylabel='Modelled {} ({})'.format('Temperature', u'\N{DEGREE SIGN}C'))
+axs[2].spines['right'].set_visible(False)
+
+axs[3].plot(psu_1208.age_ka, psu_1208.d18O_sw, color=colour_1208, linestyle='-', alpha=1)
+axs[3].fill_between(psu_1208.age_ka, psu_1208.d18O_sw_min1, psu_1208.d18O_sw_add_1, alpha=0.1, facecolor=colour_1208)
+axs[3].plot(psu_1209.age_ka, psu_1209.d18O_sw, color=colour_1209, linestyle='-', alpha=1)
+axs[3].fill_between(psu_1209.age_ka, psu_1209.d18O_sw_min1, psu_1209.d18O_sw_add_1, alpha=0.1, facecolor=colour_1209)
+axs[3].set(ylabel='Modelled {} ({})'.format(r'$\delta^{18}$O$_{sw}$', u"\u2030"))
+axs[3].yaxis.set(ticks_position="right", label_position='right')
+axs[3].spines['left'].set_visible(False)
+
+for ax in axs:
+    ax.spines['top'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+
+axs[3].spines['bottom'].set_visible(True)
+axs[3].set(xlabel='Age (ka)', xlim=[min_age, max_age])
+
+
+# Comparison Plot of Temperature against d18O
+fig, axs = plt.subplots(2, 1, sharex=True)
+# Remove horizontal space between axes
+fig.subplots_adjust(hspace=0)
+
+fig.suptitle("Comparison of {} and Modelled Temperatures".format(r'$\delta^{18}$O'))
+
+axs[0].plot(ox_1208.age_ka, ox_1208.d18Oadj, marker='+', color=colour_1208, label='ODP 1208')
+axs[0].plot(ox_1209.age_ka, ox_1209.d18Oadj, marker='+', color=colour_1209, label="ODP 1209")
+axs[0].legend(loc='upper left', shadow=False, frameon=False, bbox_to_anchor=(0.90, 1.05))
+axs[0].set(ylabel='{} ({})'.format(r'$\delta^{18}$O', u"\u2030"), xlabel='Age (ka)')
+axs[0].invert_yaxis()
+axs[0].spines['right'].set_visible(False)
+
+
+axs[1].plot(psu_1208.age_ka, psu_1208.temp, color=colour_1208, linestyle='-', alpha=1)
+axs[1].fill_between(psu_1208.age_ka, psu_1208.temp_min1, psu_1208.temp_add1, alpha=0.1, facecolor=colour_1208)
+axs[1].plot(psu_1209.age_ka, psu_1209.temp, color=colour_1209, linestyle='-', alpha=1)
+axs[1].fill_between(psu_1209.age_ka, psu_1209.temp_min1, psu_1209.temp_add1, alpha=0.1, facecolor=colour_1209)
+axs[1].set(ylabel='Modelled {} ({})'.format('Temperature', u'\N{DEGREE SIGN}C'))
+axs[1].yaxis.set(ticks_position="right", label_position='right')
+axs[1].spines['left'].set_visible(False)
+
+for ax in axs:
+    ax.spines['top'].set_visible(False)
+    ax.spines['bottom'].set_visible(False)
+
+axs[1].spines['bottom'].set_visible(True)
+axs[1].set(xlabel='Age (ka)', xlim=[min_age, max_age])
+
+plt.show()
--- methods/psu_plots/psu_plotting_01.py
+++ methods/psu_plots/psu_plotting_01.py
@@ -8,22 +8,42 @@ os.chdir("../..")
 psu_1208 = pd.read_csv("data/PSU_Solver/PSU_data_1208.csv")
 psu_1209 = pd.read_csv("data/PSU_Solver/PSU_data_1209.csv")
 
-fig, axs = plt.subplots(2, 1, sharex=True)
+temp_min, temp_max = -8, 8
+age_min, age_max = 2400, 3300
+
+# noinspection PyTypeChecker
+fig, axs = plt.subplots(3, 1, sharex=True)
 # Remove horizontal space between axes
 fig.subplots_adjust(hspace=0)
 
 
 axs[0].plot(psu_1208.age_ka, psu_1208.temp, color='green', linestyle='-', alpha=0.5)
 axs[0].fill_between(psu_1208.age_ka, psu_1208.temp_min1, psu_1208.temp_add1, alpha=0.1, facecolor='green')
-axs[0].fill_between(psu_1208.age_ka, psu_1208.temp_min2, psu_1208.temp_add2, alpha=0.05, facecolor='green')
+# axs[0].fill_between(psu_1208.age_ka, psu_1208.temp_min2, psu_1208.temp_add2, alpha=0.05, facecolor='green')
 
 
 axs[1].plot(psu_1209.age_ka, psu_1209.temp, color='red', linestyle='-', alpha=0.5)
 axs[1].fill_between(psu_1209.age_ka, psu_1209.temp_min1, psu_1209.temp_add1, alpha=0.1, facecolor='red')
-axs[1].fill_between(psu_1209.age_ka, psu_1209.temp_min2, psu_1209.temp_add2, alpha=0.05, facecolor='red')
+# axs[1].fill_between(psu_1209.age_ka, psu_1209.temp_min2, psu_1209.temp_add2, alpha=0.05, facecolor='red')
+
+axs[2].plot(psu_1208.age_ka, psu_1208.temp, color='green', linestyle='-', alpha=1)
+axs[2].plot(psu_1209.age_ka, psu_1209.temp, color='red', linestyle='-', alpha=1)
 
 for ax in axs:
-    ax.set(xlim=[2400, 3300], ylim=[-8, 8], ylabel='Temperature ({}C)'.format(u'\N{DEGREE SIGN}'),
-           yticks=np.arange(-7, 8, 2))
+    ax.set(xlim=[age_min, age_max], ylim=[temp_min, temp_max], ylabel='Temperature ({}C)'.format(u'\N{DEGREE SIGN}'),
+           yticks=np.arange(temp_min + 1, temp_max, 2))
+
+axs[2].set(ylim=[-3, 4], yticks=np.arange(-3, 4, 1))
+
+fig, ax = plt.subplots()
+ax.plot(psu_1208.age_ka, psu_1208.temp, color='green', linestyle='-', alpha=0.5)
+ax.fill_between(psu_1208.age_ka, psu_1208.temp_min1, psu_1208.temp_add1, alpha=0.05, facecolor='green')
+# axs[0].fill_between(psu_1208.age_ka, psu_1208.temp_min2, psu_1208.temp_add2, alpha=0.05, facecolor='green')
+
+ax.plot(psu_1209.age_ka, psu_1209.temp, color='red', linestyle='-', alpha=0.5)
+ax.fill_between(psu_1209.age_ka, psu_1209.temp_min1, psu_1209.temp_add1, alpha=0.05, facecolor='red')
+# axs[1].fill_between(psu_1209.age_ka, psu_1209.temp_min2, psu_1209.temp_add2, alpha=0.05, facecolor='red')
+
+
 
 plt.show()

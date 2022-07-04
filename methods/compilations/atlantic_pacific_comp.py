949c975e9c5c00aa0f51c42490d880a996b3285a
FmDG <f.m.degraaf@qmul.ac.uk>
2022-07-04 15:10:33 +0100

    ADD MgCa COMPARISON For looking at 607 and 1208/1209 we have compared it to the Mg/Ca data as well as the BWT and d18O_sw

--- methods/compilations/atlantic_pacific_comp.py
+++ methods/compilations/atlantic_pacific_comp.py
@@ -2,31 +2,68 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
-# Change to the correct directory
-os.chdir("../..")
 
-# Load the colour palette
-colours = ['#1b9e77', '#d95f02', '#7570b3', '#e7298a', '#66a61e']
+def compare_607_1209_1208():
+    # Change to the correct directory
+    os.chdir("../..")
 
-# Set the age limits
-age_min, age_max = 2400, 3200
+    # Load the colour palette
+    colours = ['#1b9e77', '#d95f02', '#7570b3']
 
-# Load the datasets
-te_1209 = pd.read_csv("data/comparisons/1209_te.csv")
-te_1208 = pd.read_csv("data/comparisons/1208_te.csv")
-te_607 = pd.read_csv("data/comparisons/607_te.csv")
-te_u1313 = pd.read_csv("data/comparisons/U1313_te.csv")
+    # Set the age limits
+    age_min, age_max = 2400, 2900
 
-# Set up the figure
-fig, axs = plt.subplots(1, 1)
+    # Load the temperature datasets
+    temp_1209 = pd.read_csv("data/PSU_Solver/PSU_data_1209.csv")
+    temp_1208 = pd.read_csv("data/PSU_Solver/PSU_data_1208.csv")
+    te_1209 = pd.read_csv("data/comparisons/1209_te.csv")
+    te_1208 = pd.read_csv("data/comparisons/1208_te.csv")
+    temp_607 = pd.read_csv("data/comparisons/607_te.csv")
 
-axs.plot(te_1209.age_ka, te_1209.MgCa, marker='+', color=colours[1], label="1209")
-axs.plot(te_1208.age_ka, te_1208.MgCa, marker='+', color=colours[2], label="1208")
-axs.plot(te_607.age_ka, te_607.MgCa, marker='+', color=colours[3], label="607")
-axs.plot(te_u1313.age_ka, te_u1313.MgCa, marker='+', color=colours[4], label="U1313")
+    # Set up the figure
+    num_plots = 3
 
-axs.legend()
+    fig, axs = plt.subplots(num_plots, 1, sharex=True, figsize=(8.25, 11.75))
+    # Name the Plots
+    fig.suptitle("Comparison of Sites 1208/1209/607\n ({} - {} ka)".format(age_min, age_max))
 
-axs.set(xlim=[age_min, age_max])
+    axs[0].plot(temp_1209.age_ka, temp_1209.temp, marker='+', color=colours[0], label="1209")
+    axs[0].plot(temp_1208.age_ka, temp_1208.temp, marker='+', color=colours[1], label="1208")
+    axs[0].plot(temp_607.age_ka, temp_607.BWT, marker='+', color=colours[2], label="607")
 
-plt.show()
+    axs[1].plot(temp_1209.age_ka, temp_1209.d18O_sw, marker='+', color=colours[0], label="1209")
+    axs[1].plot(temp_1208.age_ka, temp_1208.d18O_sw, marker='+', color=colours[1], label="1208")
+    axs[1].plot(temp_607.age_ka, temp_607.d18O_sw, marker='+', color=colours[2], label="607")
+
+    axs[2].plot(te_1209.age_ka, te_1209.MgCa, marker='+', color=colours[0], label="1209")
+    axs[2].plot(te_1208.age_ka, te_1208.MgCa, marker='+', color=colours[1], label="1208")
+    axs[2].plot(temp_607.age_ka, temp_607.MgCa, marker='+', color=colours[2], label="607")
+
+    axs[0].set(ylabel="BWT ({})".format(u'\N{DEGREE SIGN}C'))
+
+    axs[1].set(ylabel='Modelled {} ({})'.format(r'$\delta^{18}$O$_{sw}$', u"\u2030"))
+
+    axs[2].set(ylabel="Mg/Ca ({})".format('mol/mol'))
+
+    for q in range(num_plots):
+        # Remove the left/right axes to make the plot look cleaner
+        if q % 2 == 1:
+            axs[q].yaxis.set(ticks_position="right", label_position='right')
+            axs[q].spines['left'].set_visible(False)
+        else:
+            axs[q].spines['right'].set_visible(False)
+        axs[q].spines['top'].set_visible(False)
+        axs[q].spines['bottom'].set_visible(False)
+
+    # Set the bottom axis on and label it with the age.
+    axs[(num_plots - 1)].spines['bottom'].set_visible(True)
+    axs[(num_plots - 1)].set(xlabel='Age (ka)', xlim=[age_min, age_max])
+
+    # Add a legend to the first plot
+    axs[0].legend(loc='upper left', shadow=False, frameon=False)
+
+    plt.savefig("figures/607_comparison/{}_{}-{}.pdf".format("NWPacific_607_comp", age_min, age_max), format="pdf")
+
+
+if __name__ == "__main__":
+    compare_607_1209_1208()
--- methods/compilations/compiling_for_PSU.py
+++ methods/compilations/compiling_for_PSU.py
@@ -3,24 +3,12 @@ import os
 
 os.chdir("../..")
 
-trace_data = pd.read_csv("data/1208_TraceMetals.csv")
-isotope_data = pd.read_csv('data/ODP1208_cibs.csv')
+trace_data = pd.read_csv("data/comparisons/607_te.csv")
+isotope_data = pd.read_csv('data/comparisons/607_d18O.csv')
+
+trace_data.age_ka = trace_data.age_ka.astype("int")
 
 new_data = trace_data.merge(isotope_data, how='inner', on='age_ka')
 
-new_data.to_csv("data/1208_together_cibs.csv")
-
-trace_data = pd.read_csv("data/1208_TraceMetals.csv")
-isotope_data = pd.read_csv('data/ODP1208_uvi.csv')
-
-new_data = trace_data.merge(isotope_data, how='inner', on='age_ka')
-
-new_data.to_csv("data/1208_together_uvi.csv")
-
-trace_data = pd.read_csv("data/1209_TraceMetals.csv")
-isotope_data = pd.read_csv('data/ODP1209_uvi.csv')
-
-new_data = trace_data.merge(isotope_data, how='inner', on='age_ka')
-
-new_data.to_csv("data/1209_together_uvi.csv")
+new_data.to_csv("data/607_together.csv")
 

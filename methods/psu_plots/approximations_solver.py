6566bafd6d115ec2b3c29b82c3e84205280f5925
FmDG <f.m.degraaf@qmul.ac.uk>
2022-07-04 15:29:42 +0100

    REMOVE REDUNDANT FILES

--- methods/psu_plots/approximations_solver.py
+++ methods/psu_plots/approximations_solver.py
@@ -4,66 +4,30 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 
 
-# Change to the relevant directory
-os.chdir("../..")
+def solve_approximations():
+    # Change to the relevant directory
+    os.chdir("../..")
 
-# Load the isotope data
-ox_1208 = pd.read_csv("data/ODP1208_cibs.csv")
-ox_1209 = pd.read_csv("data/ODP1209_cibs.csv")
+    # Load the modelling data
+    t_1209 = pd.read_csv("data/1209_together_cib.csv")
+    t_1208 = pd.read_csv("data/1208_together_uvi.csv")
 
-# Load the trace element data
-te_1208 = pd.read_csv("data/1208_TraceMetals.csv")
-te_1209 = pd.read_csv("data/1209_TraceMetals.csv")
+    _, axs = plt.subplots(2, 2)
 
-# Load the modelling data
-t_1209 = pd.read_csv("data/1209_together_cib.csv")
-t_1208 = pd.read_csv("data/1208_together_uvi.csv")
+    sns.scatterplot(data=t_1209, x="MgCa", y='d18O', hue="age_ka", ax=axs[0, 0])
+    axs[0, 0].set(xlabel="Mg/Ca (1209)", ylabel=r"$\delta^{18}$O (1209)")
 
-# Decide on the colours for 1208 and 1209
-colour_1209, colour_1208 = "#1b9e77", "#d95f02"
+    sns.scatterplot(data=t_1209, x="MgCa", y='d13C', hue="age_ka", ax=axs[0, 1])
+    axs[0, 1].set(xlabel="Mg/Ca (1209)", ylabel=r"$\delta^{13}$C (1209)")
 
-_, axs = plt.subplots(2, 2)
+    sns.scatterplot(data=t_1208, x="MgCa", y='d18O', hue="age_ka", ax=axs[1, 0])
+    axs[1, 0].set(xlabel="Mg/Ca (1208)", ylabel=r"$\delta^{18}$O (1208)")
 
-sns.scatterplot(data=t_1209, x="MgCa", y='d18O', hue="age_ka", ax=axs[0, 0])
-axs[0, 0].set(xlabel="Mg/Ca (1209)", ylabel="d18O (1209)")
+    sns.scatterplot(data=t_1208, x="MgCa", y='d13C', hue="age_ka", ax=axs[1, 1])
+    axs[1, 1].set(xlabel="Mg/Ca (1208)", ylabel=r"$\delta^{13}$C (1208)")
 
-sns.scatterplot(data=t_1209, x="MgCa", y='d13C', hue="age_ka", ax=axs[0, 1])
-axs[0, 1].set(xlabel="Mg/Ca (1209)", ylabel="d13C (1209)")
-
-sns.scatterplot(data=t_1208, x="MgCa", y='d18O', hue="age_ka", ax=axs[1, 0])
-axs[1, 0].set(xlabel="Mg/Ca (1208)", ylabel="d18O (1208)")
-
-sns.scatterplot(data=t_1208, x="MgCa", y='d13C', hue="age_ka", ax=axs[1, 1])
-axs[1, 1].set(xlabel="Mg/Ca (1208)", ylabel="d13C (1208)")
-
-fig, axs = plt.subplots(3, sharex=True)
-# Remove horizontal space between axes
-fig.subplots_adjust(hspace=0)
-# Name the Plot
-fig.suptitle("Comparison of Sites 1208")
+    plt.show()
 
 
-axs[0].plot(t_1208.age_ka, t_1208.MgCa, color=colour_1208, marker="+")
-axs[0].set(ylabel="Mg/Ca")
-
-axs[1].plot(t_1208.age_ka, t_1208.d18O, color=colour_1208, marker="+")
-axs[1].set(ylabel="d18O")
-
-axs[2].plot(t_1208.age_ka, t_1208.d13C, color=colour_1208, marker="+")
-axs[2].set(ylabel="d13C")
-
-for q in range(3):
-    # Remove the left/right axes to make the plot look cleaner
-    if q % 2 == 1:
-        axs[q].yaxis.set(ticks_position="right", label_position='right')
-        axs[q].spines['left'].set_visible(False)
-    else:
-        axs[q].spines['right'].set_visible(False)
-    axs[q].spines['top'].set_visible(False)
-    axs[q].spines['bottom'].set_visible(False)
-
-# Set the bottom axis on and label it with the age.
-axs[2].spines['bottom'].set_visible(True)
-axs[2].set(xlabel='Age (ka)', xlim=[2400, 3400])
-
-plt.show()
+if __name__ == "__main__":
+    solve_approximations()
--- methods/trace_elements/corruption_test.py
+++ methods/trace_elements/corruption_test.py
@@ -2,58 +2,71 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import os
 
-os.chdir("../..")
 
-# Load the trace element data
-te_1209 = pd.read_csv("data/1209_TE_Full.csv")
+def corruption_test(save_fig=False):
 
-# Load the colour palette
-colours = ['#1b9e77', '#d95f02', '#7570b3', '#e7298a', '#66a61e']
+    os.chdir("../..")
 
-# Define the number of plots
-#  figsize=(8.25, 11.75)
-num_plots = 5
-fig, axs = plt.subplots(num_plots, 1, sharex=True)
-# Remove horizontal space between axes
-fig.subplots_adjust(hspace=0)
-# Name the Plots
-fig.suptitle("Comparison of Trace Elements - 1209")
+    # Load the trace element data
+    te_1209 = pd.read_csv("data/1209_TE_Full.csv")
 
-i = 0
+    # Load the colour palette
+    colours = ['#1b9e77', '#d95f02', '#7570b3', '#e7298a', '#66a61e']
 
-axs[i].plot(te_1209.mcd, te_1209.MgCa, marker="+", color=colours[i])
-axs[i].set(ylabel="Mg/Ca (mmol/mol)")
-i += 1
+    # Define the number of plots
+    num_plots = 5
 
-axs[i].plot(te_1209.mcd, te_1209.BCa, marker="+", color=colours[i])
-axs[i].set(ylabel="B/Ca  ({}/mol)".format(r'$\mu$mol'))
-i += 1
-
-axs[i].plot(te_1209.mcd, te_1209.MnCa, marker="+", color=colours[i])
-axs[i].set(ylabel="Mn/Ca ({}/mol)".format(r'$\mu$mol'))
-i += 1
-
-axs[i].plot(te_1209.mcd, te_1209.AlCa, marker="+", color=colours[i])
-axs[i].set(ylabel="Al/Ca ({}/mol)".format(r'$\mu$mol'))
-i += 1
-
-axs[i].plot(te_1209.mcd, te_1209.FeCa, marker="+", color=colours[i])
-axs[i].set(ylabel="Fe/Ca ({}/mol)".format(r'$\mu$mol'))
-i += 1
-
-for q in range(num_plots):
-    # Remove the left/right axes to make the plot look cleaner
-    if q % 2 == 1:
-        axs[q].yaxis.set(ticks_position="right", label_position='right')
-        axs[q].spines['left'].set_visible(False)
+    if save_fig:
+        fig, axs = plt.subplots(num_plots, 1, sharex=True, figsize=(8.25, 11.75))
     else:
-        axs[q].spines['right'].set_visible(False)
-    axs[q].spines['top'].set_visible(False)
-    axs[q].spines['bottom'].set_visible(False)
+        fig, axs = plt.subplots(num_plots, 1, sharex=True)
 
-# Set the bottom axis on and label it with the age.
-axs[(num_plots - 1)].spines['bottom'].set_visible(True)
-axs[(num_plots - 1)].set(xlabel='Depth (mcd)')
+    # Remove horizontal space between axes
+    fig.subplots_adjust(hspace=0)
+    # Name the Plots
+    fig.suptitle("Comparison of Trace Elements - 1209")
 
-plt.show()
-# plt.savefig("figures/TE_and_PSU_data/corruption_test.pdf", format="pdf")
+    i = 0
+
+    axs[i].plot(te_1209.mcd, te_1209.MgCa, marker="+", color=colours[i])
+    axs[i].set(ylabel="Mg/Ca (mmol/mol)")
+    i += 1
+
+    axs[i].plot(te_1209.mcd, te_1209.BCa, marker="+", color=colours[i])
+    axs[i].set(ylabel="B/Ca  ({}/mol)".format(r'$\mu$mol'))
+    i += 1
+
+    axs[i].plot(te_1209.mcd, te_1209.MnCa, marker="+", color=colours[i])
+    axs[i].set(ylabel="Mn/Ca ({}/mol)".format(r'$\mu$mol'))
+    i += 1
+
+    axs[i].plot(te_1209.mcd, te_1209.AlCa, marker="+", color=colours[i])
+    axs[i].set(ylabel="Al/Ca ({}/mol)".format(r'$\mu$mol'))
+    i += 1
+
+    axs[i].plot(te_1209.mcd, te_1209.FeCa, marker="+", color=colours[i])
+    axs[i].set(ylabel="Fe/Ca ({}/mol)".format(r'$\mu$mol'))
+    i += 1
+
+    for q in range(num_plots):
+        # Remove the left/right axes to make the plot look cleaner
+        if q % 2 == 1:
+            axs[q].yaxis.set(ticks_position="right", label_position='right')
+            axs[q].spines['left'].set_visible(False)
+        else:
+            axs[q].spines['right'].set_visible(False)
+        axs[q].spines['top'].set_visible(False)
+        axs[q].spines['bottom'].set_visible(False)
+
+    # Set the bottom axis on and label it with the age.
+    axs[(num_plots - 1)].spines['bottom'].set_visible(True)
+    axs[(num_plots - 1)].set(xlabel='Depth (mcd)')
+
+    if save_fig:
+        plt.savefig("figures/TE_and_PSU_data/corruption_test.pdf", format="pdf")
+    else:
+        plt.show()
+
+
+if __name__ == "__main__":
+    corruption_test(save_fig=False)

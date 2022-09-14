e7c807ba0cdf1af792f9941db240885228ceb51f
frisodegraaf <f.m.degraaf@qmul.ac.uk>
2022-09-14 15:30:34 +0100

    ADDED BARPLOT

--- methods/interpolations/te_interpolations.py
+++ methods/interpolations/te_interpolations.py
@@ -1,6 +1,7 @@
 import os
 
 import matplotlib.pyplot as plt
+import numpy as np
 import pandas as pd
 
 # Define some colours
@@ -61,6 +62,38 @@ def te_changepoints(age_limit=False):
     plt.show()
 
 
+def best_changepoint(age_limit=True, start=2500, stop=2850, step=10):
+    # Load the data
+    te_1209 = pd.read_csv("data/cores/1208_te.csv")
+
+    # Split ages
+    split_ages = np.arange(start=start, stop=stop, step=step)
+    diff_means = []
+    tot_errors = []
+    diff_ages = []
+
+    # Age limit
+    if age_limit:
+        te_1209 = te_1209[te_1209.age_ka < 2900]
+
+    for x in split_ages:
+        # Return the means and standard deviations
+        after_mean, after_error, before_mean, before_error = age_split(te_1209, x)
+
+        # append various lists
+        diff_means.append(abs(before_mean - after_mean))
+        tot_errors.append(after_error + before_error)
+        diff_ages.append(x)
+
+    differences_frame = pd.DataFrame(list(zip(diff_ages, diff_means, tot_errors)),
+                                     columns=['diff_age', 'Difference', "St. dev."])
+
+    fig, ax = plt.subplots(figsize=(10, 10))
+    differences_frame.plot(x="diff_age", y=["Difference", "St. dev."], kind="bar", ax=ax)
+    ax.set(xlabel="Age (ka)", ylabel='Difference in 1209 {} before/after ({})'.format('Mg/Ca', "mmol/mol"))
+    plt.show()
+
+
 if __name__ == "__main__":
     os.chdir("../..")
-    te_changepoints(age_limit=True)
+    best_changepoint(age_limit=False, start=2600, stop=2800, step=5)
---
+++ methods/plots/isotope_plots.py
@@ -0,0 +1,54 @@
+import matplotlib.pyplot as plt
+
+
+def basic_plot(datasets, age_min=2400, age_max=3400, save_fig=False, figure_name='Figure_1', colours=None):
+
+    # load the colours
+    if colours is None:
+        colours = ['#1b9e77', '#d95f02', '#7570b3', '#e7298a', '#66a61e']
+
+    # check the number of plots
+    n_plots = len(datasets)
+    if n_plots > 5:
+        raise ValueError("Maximum number of plots = 5")
+    # Check the input is correct
+    if not (isinstance(age_min, int) and isinstance(age_max, int)):
+        raise ValueError("Age inputs must be integers")
+    for entry in datasets:
+        if len(entry) != 3:
+            raise ValueError('Dataset entries to be formatted as follows\n[Series(age_ka), Series(value), Name(Value)]')
+        if len(entry[0]) != len(entry[1]):
+            raise ValueError('Age_ka and Value are not of the same length')
+
+    # Define the plot
+    if save_fig:
+        fig, axs = plt.subplots(nrows=n_plots, ncols=1, figsize=(19.6, 16.7), sharex='all')
+    else:
+        fig, axs = plt.subplots(nrows=n_plots, ncols=1, sharex='all')
+    # Remove horizontal space between axes
+    fig.subplots_adjust(hspace=0)
+
+    for x in range(n_plots):
+        axs[x].plot(datasets[x][0], datasets[x][1], color=colours[x])
+        axs[x].set(ylabel=datasets[x][2])
+
+    # Remove the various axes to clean up the plot
+    for q in range(n_plots):
+        # Remove the left/right axes to make the plot look cleaner
+        if q % 2 == 1:
+            axs[q].yaxis.set(ticks_position="right", label_position='right')
+            axs[q].spines['left'].set_visible(False)
+        else:
+            axs[q].spines['right'].set_visible(False)
+        axs[q].spines['top'].set_visible(False)
+        axs[q].spines['bottom'].set_visible(False)
+
+    # Set the bottom axis on and label it with the age.
+    axs[(n_plots - 1)].spines['bottom'].set_visible(True)
+    axs[(n_plots - 1)].set(xlabel='Age (ka)', xlim=[age_min, age_max])
+
+    if save_fig:
+        plt.savefig("figures/basic_plots/{}.png".format(figure_name), format='png')
+    else:
+        plt.show()
+

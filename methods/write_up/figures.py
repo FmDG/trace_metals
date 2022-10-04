41ed1889c1485da2a3b9d5d088cc01156f431adc
frisodegraaf <f.m.degraaf@qmul.ac.uk>
2022-10-04 10:11:48 +0100

    IMPROVED IMPORTS

--- methods/write_up/figures.py
+++ methods/write_up/figures.py
@@ -25,21 +25,6 @@ fill_607 = {'facecolor': clr[2], 'alpha': 0.1, 'label': r'$\pm 1\sigma$'}
 args_1313 = {'color': clr[3], 'label': "1313", 'marker': '+'}
 fill_1313 = {'facecolor': clr[3], 'alpha': 0.1, 'label': r'$\pm 1\sigma$'}
 
-os.chdir("../..")
-
-# Load the datasets
-# Load the PSU datasets
-psu_1208 = pd.read_csv("data/cores/1208_psu.csv").dropna()
-psu_1209 = pd.read_csv("data/cores/1209_psu.csv").dropna()
-psu_607 = pd.read_csv("data/cores/607_psu.csv").dropna()
-psu_1313 = pd.read_csv("data/cores/U1313_psu.csv").dropna()
-
-# Load the Oxygen Isotope datasets
-iso_1208 = pd.read_csv("data/cores/1208_cibs.csv")
-iso_1209 = pd.read_csv('data/cores/1209_cibs.csv')
-iso_607 = pd.read_csv("data/cores/607_cibs.csv")
-iso_1313 = pd.read_csv("data/cores/U1313_cibs_adj.csv")
-
 
 def figure_one(save_fig: bool = False) -> int:
     """
@@ -305,8 +290,111 @@ def figure_three(save_fig: bool = False) -> int:
     return 1
 
 
-if __name__ == "__main__":
-    figure_one(save_fig=False)
-    figure_two(save_fig=False)
-    figure_three(save_fig=False)
+def figure_s1(save_fig: bool = False) -> int:
+    """
+    A global context for these results, comparing the BWT in the North Atlantic (U1308/607) and the North Pacific.
+    :param save_fig: bool - save the figure to the folder paper as figure 3
+    :return: returns 1 if all is well
+    """
 
+    # Error handling
+    if not isinstance(save_fig, bool):
+        raise ValueError("save_fig must be of type bool")
+
+    # BWT across N. Atlantic and Pacific as well as d18O, and d18O_sw
+    num_plots = 2
+    min_age, max_age = 2400, 2900
+
+    # --------------------- PLOT THE DATA ------------------------
+
+    fig, axs = plt.subplots(
+        nrows=num_plots,
+        ncols=1,
+        sharex="all",
+        figsize=(8, 12)
+    )
+
+    # Remove horizontal space between axes
+    fig.subplots_adjust(hspace=0)
+    # Name the Plot
+    fig.suptitle("Global Context\n ({} - {} ka)".format(min_age, max_age))
+
+    # -- Plot the 1208 data --
+    # PSU BWT estimates
+    axs[0].plot(psu_1208.age_ka, psu_1208.temp, **args_1208)
+    axs[0].fill_between(psu_1208.age_ka, psu_1208.temp_min1, psu_1208.temp_plus1, **fill_1208)
+    # d18O isotope plot
+    axs[1].plot(iso_1208.age_ka, iso_1208.d18O_unadj, **args_1208)
+
+    # -- Plot the 1209 data --
+    # PSU BWT estimates
+    axs[0].plot(psu_1209.age_ka, psu_1209.temp, **args_1209)
+    axs[0].fill_between(psu_1209.age_ka, psu_1209.temp_min1, psu_1209.temp_plus1, **fill_1209)
+    # d18O isotope plot
+    axs[1].plot(iso_1209.age_ka, iso_1209.d18O_unadj, **args_1209)
+
+    # -- Plot the 607 data --
+    # PSU BWT estimates
+    axs[0].plot(psu_607.age_ka, psu_607.temp, **args_607)
+    axs[0].fill_between(psu_607.age_ka, psu_607.temp_min1, psu_607.temp_plus1, **fill_607)
+    # d18O isotope plot
+    axs[1].plot(iso_607.age_ka, iso_607.d18O, **args_607)
+
+    # -- Plot the U1313 data --
+    # PSU BWT estimates
+    axs[0].plot(psu_1313.age_ka, psu_1313.temp, **args_1313)
+    axs[0].fill_between(psu_1313.age_ka, psu_1313.temp_min1, psu_1313.temp_plus1, **fill_1313)
+    # d18O isotope plot
+    axs[1].plot(iso_1313.age_ka, (iso_1313.d18O - 0.64), **args_1313)
+
+    # -- Define the axes --
+    axs[0].set(ylabel="BWT ({})".format(u'\N{DEGREE SIGN}C'))
+    axs[1].set(ylabel="{} (VPDB {})".format(r'$\delta^{18}$O', u"\u2030"))
+
+    # Invert the axes with d18O
+    axs[1].invert_yaxis()
+
+    for q in range(num_plots):
+        # Remove the left/right axes to make the plot look cleaner
+        if q % 2 == 1:
+            axs[q].yaxis.set(ticks_position="right", label_position='right')
+            axs[q].spines['left'].set_visible(False)
+        else:
+            axs[q].spines['right'].set_visible(False)
+        axs[q].spines['top'].set_visible(False)
+        axs[q].spines['bottom'].set_visible(False)
+        axs[q].legend(shadow=False, frameon=False)
+
+    # Set the bottom axis on and label it with the age.
+    axs[(num_plots - 1)].spines['bottom'].set_visible(True)
+    axs[(num_plots - 1)].set(xlabel='Age (ka)', xlim=[min_age, max_age])
+
+    # Save the figure if required
+    if save_fig:
+        plt.savefig("figures/paper/Figure_S1.png".format(min_age, max_age), format="png", dpi=300)
+    else:
+        plt.show()
+
+    return 1
+
+
+if __name__ == "__main__":
+    os.chdir("../..")
+
+    # Load the datasets
+    # Load the PSU datasets
+    psu_1208 = pd.read_csv("data/cores/1208_psu.csv").dropna()
+    psu_1209 = pd.read_csv("data/cores/1209_psu.csv").dropna()
+    psu_607 = pd.read_csv("data/cores/607_psu.csv").dropna()
+    psu_1313 = pd.read_csv("data/cores/U1313_psu.csv").dropna()
+
+    # Load the Oxygen Isotope datasets
+    iso_1208 = pd.read_csv("data/cores/1208_cibs.csv")
+    iso_1209 = pd.read_csv('data/cores/1209_cibs.csv')
+    iso_607 = pd.read_csv("data/cores/607_cibs.csv")
+    iso_1313 = pd.read_csv("data/cores/U1313_cibs_adj.csv")
+
+    # figure_one(save_fig=False)
+    # figure_two(save_fig=False)
+    # figure_three(save_fig=False)
+    figure_s1(save_fig=False)

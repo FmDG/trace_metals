8cd54e904f1f12be45da729d027d73d547853be6
frisodegraaf <f.m.degraaf@qmul.ac.uk>
2023-07-11 17:13:49 +0100

    ADDED PROBSTACK Added the probstack file to the comparisons data set and then added a code file to compare age models to probStack.csv

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from methods.simple_figures.bwt_figure import iso_bwt_figure
+from methods.compilations.probStack_comparison import probStack_comparison
 
 
 if __name__ == "__main__":
-    iso_bwt_figure(age_min=2400, age_max=2900)
+    probStack_comparison()
 
